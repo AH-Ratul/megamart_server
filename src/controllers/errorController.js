68ad6692bc879c577c640b2e16c18d21465019bb
AH-Ratul <ahratul21@gmail.com>
2024-08-06 16:39:44 +0600

    updating errors

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,57 +1,62 @@
-const handleDuplicateField = err => {
-    const value = err.errmsg.match(/(["'])(\\?.)*?\1/);
-    console.log(value)
-    const message = `Duplicate Field value ${value}. please use another value.`;
-  }
-  
-  const sendErrorDev = (err, res) => {
+const AppError = require("../utils/appError");
+
+const handleDuplicateField = (err) => {
+  const regex = /(["'])(\\?.)*?\1/;
+  const value = err.errorResponse.errmsg.match(regex)[0];
+  console.log(value);
+
+  const message = `Duplicate value ${value}. please use another value.`;
+  return new AppError(message, 400);
+};
+
+const handleJWTEpiresError = (error) => {
+
+  return new AppError("Your Session is Expired. Please Login Again", 401);
+}
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational , trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  };
-  
-  // const sendErrorProd = (err, res) => {
-  //   // Operational , trusted error: send message to client
-  //   if (err.isOperational) {
-  //     res.status(err.statusCode).json({
-  //       status: err.status,
-  //       message: err.message,
-  //     });
-  
-  //     // programming or other unknown errors
-  //   } else {
-  //     // log error
-  //     console.error('Error 🔥',err)
-  
-  //     // send generic message
-  //     res.status(500).json({
-  //       status: "error",
-  //       message: "something went very wrong!!",
-  //     });
-  //   }
-  // };
-  
-  module.exports = (err, __, res, ___) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "Internal Server Error";
-  
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
       message: err.message,
       stack: err.stack
     });
-  
-    // if (process.env.NODE_ENV === "development") {
-    //   sendErrorDev(err, res);
-    // } else if (process.env.NODE_ENV === "production") {
-    //   let error = {...err};
-  
-    //   if(error.code === 11000) error = handleDuplicateField(error);
-    //   sendErrorProd(error, res);
-    // }
-  };
-  
+
+    // programming or other unknown errors
+  } else {
+    // log error
+    console.error("Error 🔥", err);
+
+    // send generic message
+    res.status(500).json({
+      status: "error",
+      message: "something went very wrong!!",
+    });
+  }
+};
+
+module.exports = (err, __, res, ___) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "Internal Server Error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+
+    if (error.errorResponse && error.errorResponse.code === 11000) error = handleDuplicateField(error);
+    if (error.name === "TokenExpiredError") error = handleJWTEpiresError(error);
+
+    sendErrorProd(error, res);
+  }
+};
