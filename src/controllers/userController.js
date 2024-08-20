3b6a7b8a7f53dae35987bd1af592c58dca6df7f4
AH-Ratul <ahratul21@gmail.com>
2024-08-20 19:11:52 +0600

    2 new functions added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,3 +15,29 @@ exports.getUser = tryCatch(async (__, res, next) => {
     data: get,
   });
 });
+
+exports.getUserById = tryCatch(async (req, res, next) => {
+  const getById = await User.findById(req.params.id);
+
+  if (!getById) {
+    return next(new AppError("User not found by this ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: getById,
+  });
+});
+
+exports.deleteUser = tryCatch(async (req, res, next) => {
+  const delUser = await User.findByIdAndDelete(req.params.id);
+
+  if (!delUser) {
+    return next(new AppError("User not found by this ID to delete", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "User deleted",
+  });
+});
