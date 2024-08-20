edb5d5ecbc3aeda49afdaafce96a108d58522bd1
AH-Ratul <ahratul21@gmail.com>
2024-08-20 11:14:44 +0600

    added user route

--- src/routes/userRouter/userRouter.js
+++ src/routes/userRouter/userRouter.js
@@ -1,9 +1,9 @@
 const userRouter = require("express").Router();
 const authController = require("../../controllers/authController");
+const userController = require("../../controllers/userController");
 
 userRouter.post("/signup", authController.signup);
-userRouter.post('/login', authController.login);
-userRouter.get("/get",authController.protect, authController.getUser);
+userRouter.post("/login", authController.login);
+userRouter.get("/get", authController.protect, userController.getUser);
 
 module.exports = userRouter;
-
