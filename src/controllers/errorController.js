84355115fc236ef86b779986de7d11942f55361e
AH-Ratul <ahratul21@gmail.com>
2024-08-24 23:29:58 +0600

    fix issues

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,28 +1,32 @@
 const AppError = require("../utils/appError");
 
+// HANDLE INVALID ID'S
+const handleCastError = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// HANDLE DUPLICATE FIELD VALUE
+const handleDuplicateField = (err) => {
+  const regex = /(["'])(\\?.)*?\1/;
+  const value = err.errorResponse.errmsg.match(regex)[0];
+
+  const message = `Duplicate value ${value}. please use another value.`;
+  return new AppError(message, 400);
+};
+
+// HANDLE MONGOOSE VALIDATION ERROR
 const handleValidationError = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
   const message = `${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
-const handleCastError = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}`;
-  return new AppError(message, 400);
-};
-
-const handleDuplicateField = (err) => {
-  const regex = /(["'])(\\?.)*?\1/;
-  const value = err.errorResponse.errmsg.match(regex)[0];
-  console.log(value);
-
-  const message = `Duplicate value ${value}. please use another value.`;
-  return new AppError(message, 400);
-};
-
+// HANDLE JWT ERROR
 const handleJwtError = () =>
   new AppError("Invalid Token, Please login again", 401);
 
+// HANDLE JWT TOKEN EXPIRES
 const handleJWTEpiresError = () =>
   new AppError("Your Session is Expired. Please Login Again", 401);
 
@@ -38,11 +42,14 @@ const sendErrorDev = (err, res) => {
 
 // SEND VALID ERROR MESSAGE IN PRODUCTION
 const sendErrorProd = (err, res) => {
+  // Send trusted error message
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
+
+    // send generic message, don't leak error details to user
   } else {
     console.error("Error 🔥", err);
     res.status(500).json({
@@ -59,18 +66,19 @@ module.exports = (err, __, res, ___) => {
   if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
-    let error = { ...err };
-    error.message = err.message;
+    //let error = { ...err };
+    err.message = err.message;
 
-    if (error.name === "ValidationError" || error.statusCode === 500)
-      error = handleValidationError(error);
-    if (error.name === "CastError" || error.statusCode === 500)
-      error = handleCastError(error);
-    if (error.errorResponse && error.errorResponse.code === 11000)
-      error = handleDuplicateField(error);
-    if (error.name === "JsonWebTokenError") error = handleJwtError(error);
-    if (error.name === "TokenExpiredError") error = handleJWTEpiresError();
+    if (err.name === "CastError") err = handleCastError(err);
 
-    sendErrorProd(error, res);
+    if (err.code === 11000) err = handleDuplicateField(err);
+
+    if (err.name === "ValidationError") err = handleValidationError(err);
+
+    if (err.name === "JsonWebTokenError") err = handleJwtError();
+
+    if (err.name === "TokenExpiredError") err = handleJWTEpiresError();
+
+    sendErrorProd(err, res);
   }
 };
