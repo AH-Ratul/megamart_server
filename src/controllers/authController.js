247494e2f50046442d47d838ff8b934a32534d01
AH-Ratul <ahratul21@gmail.com>
2024-08-28 00:55:02 +0600

    auth updated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -19,11 +19,12 @@ const createSendToken = (user, statusCode, message, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 60 * 1000),
     httpOnly: true,
+    sameSite: "strict",
   };
 
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
-  res.cookie("jwt", token, cookieOptions);
+  res.cookie("token", token, cookieOptions);
 
   user.password = undefined;
 
@@ -73,6 +74,8 @@ exports.protect = tryCatch(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
