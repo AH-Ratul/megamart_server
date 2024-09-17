acda613bef9457263c53abd01a8a84026bd31696
AH-Ratul <ahratul21@gmail.com>
2024-09-17 18:52:00 +0600

    refactor server file

---
+++ server.js
@@ -0,0 +1,35 @@
+require("./src/config");
+const app = require("./src/app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+// connected to MongoDB database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log("Database Error -> ", err);
+  });
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server is Listening on Port ${PORT}`);
+});
+
+// handling uncaught expression
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UnCaughtException, Shutting down...");
+  process.exit(1);
+});
+
+// handling unhandled rejection
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UnHandledRejection, Shutting down...");
+  process.exit(1);
+});
