24f9875bb7775bad65a58da377619fabbf57e7c9
AH-Ratul <ahratul21@gmail.com>
2024-11-01 20:22:06 +0600

    add new api route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,8 @@
-const appRouter = require('express').Router();
-const userRouter = require('./userRouter/userRouter');
+const appRouter = require("express").Router();
+const userRouter = require("./userRouter/userRouter");
+const productRouter = require("./productRouter/productRouter");
 
-appRouter.use('/users', userRouter);
+appRouter.use("/users", userRouter);
+appRouter.use("/products", productRouter);
 
 module.exports = appRouter;
