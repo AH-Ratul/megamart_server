a2debca326db5a386dae5bef8d92dac23db175f9
AH-Ratul <ahratul21@gmail.com>
2024-11-01 20:23:08 +0600

    model updated

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,31 +1,68 @@
-const { Schema } = require("mongoose");
+const { Schema, default: mongoose } = require("mongoose");
 
 const productSchema = new Schema({
-    productName: {
+  productName: {
+    type: String,
+    required: [true, "Must contains a Name"],
+    unique: true,
+  },
+  brand: { type: String },
+  category: { type: String },
+  description: { type: String },
+  productImage: {
+    type: [String],
+    required: [true, "Product must contains a Image"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Product must contains a price"],
+  },
+  discountPrice: { type: Number },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  reviews: [
+    {
+      user: {
         type: String,
-        required: [true, "Must contains a Name"],
-        unique: true
-    },
-    brand: {
-        type: String
-    },
-    category: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    productImage: {
-        type: [String],
-        required: true
-    },
-    price: {
+        required: true,
+      },
+      comment: { type: String },
+      rating: {
         type: Number,
-        required: true
+        min: 0,
+        max: 5,
+      },
     },
-    discountPrice: {
-        type: Number
-    },
-    
+  ],
+  details: {
+    type: Map,
+    of: Schema.Types.Mixed,
+    required: true,
+  },
+});
 
-})
+productSchema.add({
+  details: {
+    fashion: {
+      brand: { type: String },
+      material: { type: String },
+      sizeGuide: { type: String },
+      colorOption: { type: String },
+      fit: { type: String },
+    },
+    books: {
+      author: { type: String },
+      publisher: { type: String },
+      pages: { type: String },
+      edition: { type: String },
+      genre: { type: String },
+    },
+  },
+});
+
+const Product = mongoose.model("Products", productSchema);
+
+module.exports = Product;
