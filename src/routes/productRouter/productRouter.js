165268716c6dea591989fa40bfe043d9b030a714
AH-Ratul <ahratul21@gmail.com>
2024-11-01 20:29:05 +0600

    route exports

--- src/routes/productRouter/productRouter.js
+++ src/routes/productRouter/productRouter.js
@@ -2,3 +2,5 @@ const productRouter = require("express").Router();
 const productController = require("../../controllers/productController");
 
 productRouter.post("/addProduct", productController.addProduct);
+
+module.exports = productRouter;
