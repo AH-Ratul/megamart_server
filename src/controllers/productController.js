997c9a065c57766cd512224a8532aaa88042e8fa
AH-Ratul <ahratul21@gmail.com>
2024-11-07 00:02:16 +0600

    functionality updates

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -6,5 +6,10 @@ const AppError = require("../utils/appError");
 exports.addProduct = tryCatch(async (req, res) => {
   const productData = req.body;
 
-  
+  const newProduct = new Product(productData);
+  await newProduct.save();
+
+  res
+    .status(201)
+    .json({ status: "success", message: "added", data: newProduct });
 });
