3d44c734be4039ee27fe4609d42d1a9f5541897f
AH-Ratul <ahratul21@gmail.com>
2024-11-08 01:12:02 +0600

    cors update

--- src/app.js
+++ src/app.js
@@ -21,7 +21,12 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieparser());
 
