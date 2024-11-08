d15e244a2d610d7068dd01ee5f84101d6fc3ddb2
AH-Ratul <ahratul21@gmail.com>
2024-11-08 21:21:59 +0600

    perform on GET products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,7 @@ const Product = require("../models/productModel");
 const tryCatch = require("../utils/tryCatch");
 const AppError = require("../utils/appError");
 
-// ADD PRODUCT
+//---------------- ADD PRODUCT ---------------------
 exports.addProduct = tryCatch(async (req, res) => {
   const productData = req.body;
 
@@ -12,4 +12,15 @@ exports.addProduct = tryCatch(async (req, res) => {
   res
     .status(201)
     .json({ status: "success", message: "added", data: newProduct });
+});
+
+//---------------- GET PRODUCT----------------------
+exports.getProduct = tryCatch(async (req, res) => {
+  const getProducts = await Product.find();
+
+  res.status(200).json({
+    status: "success",
+    results: getProducts.length,
+    data: getProducts,
+  });
 });
