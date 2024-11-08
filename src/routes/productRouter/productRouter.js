3b087cdd85ac8943c2868359608aaf09837062e0
AH-Ratul <ahratul21@gmail.com>
2024-11-08 21:20:56 +0600

    handle route to GET products

--- src/routes/productRouter/productRouter.js
+++ src/routes/productRouter/productRouter.js
@@ -2,5 +2,6 @@ const productRouter = require("express").Router();
 const productController = require("../../controllers/productController");
 
 productRouter.post("/addProduct", productController.addProduct);
+productRouter.get("/getProducts", productController.getProduct);
 
 module.exports = productRouter;
