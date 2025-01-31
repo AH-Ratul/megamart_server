e58c70b3ed5aa57a1b5bc71845377ddec8024878
AH-Ratul <ahratul21@gmail.com>
2025-01-31 19:11:00 +0600

    handle route with parameter

--- src/routes/productRouter/productRouter.js
+++ src/routes/productRouter/productRouter.js
@@ -3,5 +3,6 @@ const productController = require("../../controllers/productController");
 
 productRouter.post("/addProduct", productController.addProduct);
 productRouter.get("/getProducts", productController.getProduct);
+productRouter.get("/getProducts/:id", productController.getProductByID);
 
 module.exports = productRouter;
