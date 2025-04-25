96cffabc0b77f36a766e515f03f31770aa2b178b
AH-Ratul <ahratul21@gmail.com>
2025-04-26 00:25:04 +0600

    contact route added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,11 @@ const appRouter = require("express").Router();
 const userRouter = require("./userRouter/userRouter");
 const productRouter = require("./productRouter/productRouter");
 const cartRouter = require("./cartRouter/cartRouter");
+const contactRouter = require("./contactRouter/contactRouter");
 
 appRouter.use("/users", userRouter);
 appRouter.use("/products", productRouter);
 appRouter.use("/cart", cartRouter);
+appRouter.use("/contact", contactRouter);
 
 module.exports = appRouter;
