2e440d4f721b648c929c96337f207e49a2718373
AH-Ratul <ahratul21@gmail.com>
2025-04-27 22:14:08 +0600

    new route added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,12 @@ const userRouter = require("./userRouter/userRouter");
 const productRouter = require("./productRouter/productRouter");
 const cartRouter = require("./cartRouter/cartRouter");
 const contactRouter = require("./contactRouter/contactRouter");
+const orderRouter = require("./orderRouter/orderRouter");
 
 appRouter.use("/users", userRouter);
 appRouter.use("/products", productRouter);
 appRouter.use("/cart", cartRouter);
 appRouter.use("/contact", contactRouter);
+appRouter.use("/order", orderRouter);
 
 module.exports = appRouter;
